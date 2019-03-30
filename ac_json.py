c59602fd13e8025b2424b6df398665660ab6a95f
ruckyboy <ruckyboy@gmail.com>
2019-03-30 20:00:47 +0800

    added sims parsing

--- ac_json.py
+++ ac_json.py
@@ -1,6 +1,7 @@
 import json
 import requests
 import time
+from urllib import parse
 from datetime import datetime
 import ac_utility
 from ac_constants import *
@@ -11,6 +12,58 @@ from ac_constants import *
     # Benefits: local data is persistent and doesn't rely on internet connection
     # Airtable data can be parsed, filtered and then well formed before being written local
 """
+
+sims_root = "https://applicant.sims.uwa.edu.au/connect/webconnect?" \
+            "pagecd=UPTMTBL&dataname=%7Cp_mode%7Cp_ret%7Cp_draft_ind%7Cp_uoos"
+sims_query = "&datavalue=%7CVENUE%7CDTL%7CY%7CARTS%3A+%5B++G59%5D+Fox+Lecture+Hall"
+
+# sims_query = parse.quote_plus('&datavalue=|VENUE|GRD|Y|ARTS: [  G59]', safe='/&=')
+
+
+def load_sims(url=sims_root, options=sims_query):
+    """ sims returns a jquery? object of all room booking items (dictionaries) """
+
+    try:
+        pulljson = requests.get(url + options)
+        status = pulljson.status_code
+    except (requests.Timeout, requests.ConnectionError, KeyError, Exception) as e:
+        print(f'Exception raised: {e}')
+        pulljson = {}
+        status = 408
+    return pulljson, status
+
+
+def build_sims_json():
+    req, json_request_status = load_sims()
+    if json_request_status == requests.codes.ok:
+
+        current_week = datetime.now().date().isocalendar()[1]
+
+        bookings_list = []
+        cleaned_response = (req.text[1:-11])  # remove cruft from top and tail of response
+        loaded = json.loads(cleaned_response)
+        # the return from the query is a mish mash of stuff, not json, no consistency in formatting, etc  X:(
+        for booking in loaded[1:]:  # dump the first record - just structure info
+            bookingdetail = {}
+            bookingdetail["title"] = booking.get("actLongFrm", "MISSING")
+            bookingdetail["day"] = booking.get("day", "MISSING")
+            bookingdetail["duration"] = booking.get("sttoend", "MISSING")
+            bookingdetail["weeknos"] = booking.get("wknos", "MISSING")
+            weeks = bookingdetail["weeknos"].split(',')
+            while "" in weeks:
+                weeks.remove("")
+            bookingdetail["weeks"] = list(map(int, weeks))
+
+            if current_week in bookingdetail["weeks"]:
+                bookings_list.append(bookingdetail)
+
+        temp_json_file = DATA_DIR / "sims.json"
+        with temp_json_file.open("w") as file:
+            json.dump(bookings_list, file, indent=2)
+
+    else:
+        print(f'Failed to connect to sims\n\n({json_request_status})')
+        print(ERROR_CODES.get(str(json_request_status), 'Error code not found'))
 
 
 def load_airtable(bearer_key=None, url=None, options=None, offset=""):
@@ -52,9 +105,7 @@ def build_icandi_json():
     url = tmp_prefs["airtable_url"]
     options = ""
     # options = "&maxRecords=11&filterByFormula=(Group='Business School')"  # just for testing
-    # options = "&maxRecords=11&filterByFormula=(Group='Arts')"  # just for testing
-    # options = "&maxRecords=11&filterByFormula=(Group='Physics')"  # just for testing
-    # options = "&maxRecords=11&filterByFormula=(Group='Engineering')"  # just for testing
+
     del tmp_prefs
 
     while morepages:
@@ -90,8 +141,8 @@ def build_icandi_json():
                         venue["capacity"] = int(fields.get("Capacity", 0))
                         venue["group"] = fields.get("Group", "Unknown")
                         venue["phone"] = str(fields.get("Phone", "Unknown"))
-                        venue["ctf"] = "Yes" if fields.get("CTF") else "No"         # AirTable stored as boolean
-                        venue["cardax"] = "Yes" if fields.get("Cardax") else "No"   # AirTable stored as boolean
+                        venue["ctf"] = "Yes" if fields.get("CTF") else "No"  # AirTable stored as boolean
+                        venue["cardax"] = "Yes" if fields.get("Cardax") else "No"  # AirTable stored as boolean
                         venue["notes"] = fields.get("Notes", "")
                         venue["pc"] = fields.get("_PC", "")
                         venue["radmicfreq"] = fields.get("_Radio Mic Frequency", "")
@@ -113,7 +164,7 @@ def build_icandi_json():
                                 devicelist.append(d.split('; '))
 
                         # add, pc, web cam and echo360 to the device/ip collection
-                        pc = fields.get("_PC","")
+                        pc = fields.get("_PC", "")
                         webcam = fields.get("_WebCam", "")
                         echo360 = fields.get("_Echo360", "")
 
@@ -175,5 +226,9 @@ def build_icandi_json():
 
 
 if __name__ == '__main__':
-    is_rebuilt, msg = build_icandi_json()
-    print(f'Database updated by build_icandi_json: {is_rebuilt}\nPassed Message: {msg}')
+    # is_rebuilt, msg = build_icandi_json()
+    # print(f'Database updated by build_icandi_json: {is_rebuilt}\nPassed Message: {msg}')
+
+    build_sims_json()
+
+    pass
